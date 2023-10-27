b9a4839af3bb76e96289b16ce8406f144c515ef9
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-10-27 13:28:14 +0530

    🐛 ui fix

--- src/components/MainContainer/LocationBasedRestaurants/RestaurantDetailCard/RestaurantDetailsFilterTabs/OrderOnline/RestaurantCard/RestaurantCard.jsx
+++ src/components/MainContainer/LocationBasedRestaurants/RestaurantDetailCard/RestaurantDetailsFilterTabs/OrderOnline/RestaurantCard/RestaurantCard.jsx
@@ -19,19 +19,12 @@ const RestaurantCard = ({ menu }) => {
     setResCard(menu?.menu?.categories);
   }, []);
 
-  // console.log(resCard);
-
-  // console.log(resCard[0]?.category?.items[0]?.item?.item_image_url);
-
-  // console.log(resCard[0]?.category?.id);
-
   return (
     <div className="mt-2 space-y-4">
       <h1 className="text-2xl mt-4">{menu?.menu?.name}</h1>
       <div className="mt-2">
         {resCard?.map(category => (
           <div className="mb-4">
-            {/* <h2 className="text-xl font-semibold">{category?.category?.id}</h2> */}
             <div className="flex flex-col items-left space-y-5 overflow-auto">
               {category?.category?.items?.map(item => (
                 <div
@@ -40,7 +33,7 @@ const RestaurantCard = ({ menu }) => {
                 >
                   <div>
                     <img
-                      className="w-40 h-40 rounded-lg object-fill"
+                      className="w-[160px] h-[160px] rounded-lg object-fill"
                       src={item?.item?.item_image_url}
                       alt={item?.item?.item_image_url}
                     />
